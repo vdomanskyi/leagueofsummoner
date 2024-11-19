9eb555de3527608430da39ee7fe0444d51da149d
Valenty Domanskyi <v.domanskyi@autodoc.eu>
2024-11-19 20:08:26 +0200

    Update get match list

--- dist/widget.js
+++ dist/widget.js
@@ -44,7 +44,7 @@ const requests = {
       const data =
         'https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/' +
         puuid +
-        '/ids?start=0&count=' +
+        '/ids?type=ranked&start=0&count=' +
         count +
         '&api_key=' +
         apiKey;
